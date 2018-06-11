0e435777d56d093b73f930d0f07a719ba81378d1
Katz05 <40074664+Katz05@users.noreply.github.com>
2018-06-11 14:32:06 -0500

    Add files via upload

--- proj01_ifelse/proj01.py
+++ proj01_ifelse/proj01.py
@@ -7,9 +7,31 @@
 # This program asks the user for his/her name and grade.
 #Then, it prints out a sentence that says the number of years until they graduate.
 
+# print "Hello World!"
+# fav_color = raw_input("What's your favorite color?")
+# fav_number = int(raw_input("What's your fav number?"))
+# print "Your fav color is " + fav_color + "."
+# print "5 + your favorite number =" + str(5 + fav_number) + "."
+Name = raw_input("What's your name?")
+Grade = int(raw_input("What grade are you in?"))
+print Name + ", you wil graduate from highschool in " + str(13 - Grade) + " years."
 # Part II:
 # This program asks the user for his/her name and birth month.
 # Then, it prints a sentence that says the number of days and months until their birthday
 
 
+user_number = int(raw_input("Enter a number: "))
+
+if user_number > 5:
+    print "Your number is greater than 5!"
+elif user_number == 5:
+    print "Your number is 5!"
+else:
+    print "Your number is less than 5!"
+Birthmonth = int(raw_input("What month were you born in?"))
+Birthday = int(raw_input("What day were you born on?"))
+if Birthmonth > 6
+    str(Birthmonth - 6)
+    if Birthday >= 11
+        str(Birthday - 11)
 # If you complete extensions, describe your extensions here!
