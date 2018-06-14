526bec787328a02201989b10f2d3bf301be51934
Katz05 <40074664+Katz05@users.noreply.github.com>
2018-06-14 13:35:51 -0500

    Add files via upload

--- proj05_hangman/proj05_hangman.py
+++ proj05_hangman/proj05_hangman.py
@@ -46,3 +46,68 @@ def choose_word(wordlist):
 wordlist = load_words()
 
 # your code begins here!
+
+word = choose_word(wordlist)
+
+L = []
+L2 = []
+var = string.lowercase
+counter = 10
+exit = 1
+Answer = False
+
+
+for letter in word:
+    L.append(letter)
+    L2.append("_")
+
+print "Welcome to Hangman! Type 0 to exit."
+print "I am thinking of a word " +str(len(word)) + " letters long."
+print L2
+
+
+
+while counter > 0 and exit == 1:
+    print "------------------"
+    print "Guesses: " + str(counter)
+    print "Available letters: " + var
+    user_guess = raw_input("Please enter a letter: ")
+
+    for letter in word:
+        if user_guess == letter:
+            var = var.replace(user_guess, "")
+            #print L2
+            Answer = True
+
+        elif user_guess != letter:
+            var = var.replace(user_guess, "")
+            #print "YOU'RE WRONG!"
+            #print L2
+
+
+        else:
+            counter = counter + 1
+
+
+
+
+    if user_guess == "0":
+        exit = exit - 1
+    elif Answer == False:
+        print "YOU'RE WRONG!"
+        counter = counter - 1
+    else:
+        print "Nice, you got a letter!"
+        c2 = 0
+        for letter in L:
+            on = "_"
+            nn = user_guess
+            if letter == nn:
+                L2[c2] = nn
+            c2 = c2 + 1
+    print L2
+
+
+
+
+
